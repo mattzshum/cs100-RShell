8d71b5e23f27db7d546a9f6ccd06b637e1b5f0f6
cjlee244 <38056482+cjlee244@users.noreply.github.com>
2019-03-01 00:40:07 -0800

    Create semiColon.cpp

---
+++ src/semiColon.cpp
@@ -0,0 +1,20 @@
+#include "semiColon.h"
+
+using namespace std;
+
+SemiColon::SemiColon() {};
+    
+bool SemiColon::isConnector(){
+    return true;
+}
+    
+bool SemiColon::execute() {
+    lhs->execute();
+    rhs->execute();
+    return true;
+}
+    
+void SemiColon::setBase(Base* a, Base* b){
+    lhs = a;
+    rhs = b;
+}
