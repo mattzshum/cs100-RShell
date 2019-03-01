aeb1d1e76e65eb24bd4a2ddc501641d8a9661aaa
cjlee244 <38056482+cjlee244@users.noreply.github.com>
2019-03-01 00:35:11 -0800

    update command.cpp

--- src/Command.cpp
+++ src/Command.cpp
@@ -1,68 +1,79 @@
-#include "Command.h"
+#include "command.h"
 
 using namespace std;
 
-Command::Command()
-{}
+Command::Command() {};
 
-Command::Command(char *input) {
-    cmd = input;
-    vector<char *> inlist;
-    inlist.push_back(input);
-    inlist.push_back('\0');
-    inlist.push_back('\0');
-    cmdlist = inlist;
-    //cout << "construct char" << endl;
+Command::Command(string command) : lhs(0), rhs(0) {
+    cmd = command;
 }
-
-Command::Command(vector<char *> cmdlist) {
-    this->cmdlist = cmdlist;
-        // cout << cmdlist.at(0) << endl;
-        // cout << cmdlist.at(1) << endl;
-        // cout << "construct vector" << endl;
-
-}
-
-int Command::execute() {
-    int status = 1;
-    char* args[3];
-    args[0] = cmdlist.at(0);
-    args[1] = cmdlist.at(1);
-    args[2] = (char*)'\0';
-    // char *args[2];
-    // args[0] = 
-    pid_t pid = fork();
     
-    // if (!cmd.empty()) {
-	// 		cmds.push_back(this->buildCommand(cmd));
-	// 	} else if (commands.front() == ";") {
-	// 		cmds.push_back(new Terminate()); 
-	// 		commands.pop_front();
-	// 	} else if (commands.front() == "||") {
-	// 		cmds.push_back(new Or()); 
-	// 		commands.pop_front();
-	// 	} else if (commands.front() == "&&") {
-	// 		cmds.push_back(new And());
+bool Command::isConnector() {
+    return false; 
+}
+    
+void Command::setBase(Base* a, Base* b) {
+    lhs = a;
+    rhs = b;
+}
+    
+bool Command::execute() {
+    char ** args;    
+    istringstream iss(cmd);
+    string temp = "";
+    string cmdSysCall;
+    string restCmd;
+        
+    iss >> temp;
+    cmdSysCall = temp;
+        
+    if (cmdSysCall == "exit") {
+        exit(1);
+    }
+        
+    temp = "";
+    iss >> temp;
 
-    if (pid == -1) {
-        perror("Error: fork");
+    if (temp != "") {            
+        string restCmd = cmd.substr(cmd.find_first_of(" "), cmd.length()); // check the syntax of this 
+        restCmd.erase(0, 1); 
+        args = new char*[2];
+        args[0] = (char*)cmdSysCall.c_str();
+        args[1] = (char*)restCmd.c_str();
+        args[2] = NULL;
     }
 
-    else if (pid == 0) {
-        //cout <<  "args[0" << args[0] << endl; 
-        if (execvp(args[0], args) == -1) {
-            status = 0;
-            perror("Error: execvp");
-        }
-    }
     else {
-        int check;
-        if (waitpid(pid, &check, 0) == 1) {
-            perror("Wait");
+        args = new char*[1];
+        args[0] = (char*)cmdSysCall.c_str();
+        args[1] = NULL;            
+    }
+    
+    pid_t pid = fork();
+        
+    if (pid > 0) {
+        int Status;
+            
+        if (waitpid(pid, &Status, 0) == -1){       
+            perror("wait");
         }
-        if (WEXITSTATUS(check) != 0) {
-            status = 0;
+            
+        if (WEXITSTATUS(Status) != 0) {
+            return false;
         }
     }
-    return status;
+        
+    else if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+            perror("exec");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    else{
+        perror("fork");
+        return false;
+    }
+
+    return true;
 }
