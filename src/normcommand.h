fab5d2ad42322a50dba21da7f9344c8aa6e5dd27
cjlee244 <38056482+cjlee244@users.noreply.github.com>
2019-03-02 13:21:37 -0800

    Create normcommand.h

---
+++ src/normcommand.h
@@ -0,0 +1,23 @@
+#ifndef NORMCOMMAND_H
+#define NORMCOMMAND_H
+
+#include "command.h"
+
+#include <iostream>
+
+using namespace std;
+
+class normCommand : public Command{
+  protected:
+    Base* lhs;
+    Base* rhs;
+  
+  public:
+    normCommand();
+    normCommand(string);
+    bool isConnector();
+    void setBase(Base*, Base*);
+    bool execute();
+};
+
+#endif
