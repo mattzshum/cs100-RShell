14242e6920a05fb0e7cf8ff2023025985fc231c8
cjlee244 <38056482+cjlee244@users.noreply.github.com>
2019-03-02 13:21:11 -0800

    Create connector.h

---
+++ src/connector.h
@@ -0,0 +1,28 @@
+#ifndef CONNECTOR_H
+#define CONNECTOR_H
+
+#include "base.h"
+
+using namespace std;
+
+/*
+Connectors act as the operands in lab3 and 4. They will purely execute what is passed into them and return a boolean value of whether or not it passed.
+The boolean it returns will be determined by the command it is assigned.
+*/
+
+
+
+class Connector : public Base{
+  protected:
+    Base* lhs;
+    Base* rhs;
+    
+  public:
+    Connector() : lhs(0), rhs(0) {};
+    
+    virtual bool isConnector() = 0;
+    virtual void setBase(Base* a, Base* b) = 0;
+    virtual bool execute() = 0;
+};
+
+#endif
