5c9f68ceb1ecf328cd80dc2b719ebf140659ec0b
cjlee244 <38056482+cjlee244@users.noreply.github.com>
2019-03-02 13:20:33 -0800

    Create command.h

---
+++ src/command.h
@@ -0,0 +1,37 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#include "base.h"
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+class Command : public Base{
+  protected:
+    //executablel [arg list] [connector] [cmd]
+    Base * lhs;
+    Base * rhs;
+    
+  public:
+    Command() {
+        
+    };
+    Command(string command) : lhs(0), rhs(0) {
+        cmd = command;
+    };
+    virtual bool isConnector() = 0;
+    virtual void setBase(Base*, Base*) = 0;
+    virtual bool execute() = 0;
+};
+#endif
