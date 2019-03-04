c7dc89fa5cdbc74636e0a9fc9aa6e17022c57b03
christian lee <clee244@ucr.edu>
2019-03-03 20:06:57 -0800

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
-
 #include "rshell.h"
+#include "and.h"
+#include "base.h"
+#include "command.h"
+#include "connector.h"
+#include "normcommand.h"
+#include "testcommand.h"
+#include "or.h"
+#include "semicolon.h"
+
+#include "gtest/gtest.h"
+
+#include <iostream>
 
 using namespace std;
 
