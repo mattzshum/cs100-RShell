5cc7a6d7aadef916e6d3d69ad132be4af79c5f11
cjlee244 <38056482+cjlee244@users.noreply.github.com>
2019-03-17 23:16:22 -0700

    Update pipe.h

--- src/pipe.h
+++ src/pipe.h
@@ -16,18 +16,18 @@ class Pipe : public Connector{
     string getCommand() { return "";}
     
     bool execute(int in, int out){
-        /*string outputFile = rhs->getCommand();
         string inputFile = lhs->getCommand();
+        string outputFile = rhs->getCommand();
+        in = open(inputFile.c_str(), O_RDONLY);
         out = open(outputFile.c_str(), O_WRONLY| O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
-        in = open(inputFile.c_str(), O_RDONLY);//O_WRONLY| O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
-        
-        if(!lhs->execute(in, out)){
+
+        if(!lhs->execute(in, 1)){
             return false;
         }
-        if(!rhs->execute(in, out)){
+        if(!rhs->execute(0, out)){
             return false;
         }
-        return true;*/
+        return true;
     }
 };
 
